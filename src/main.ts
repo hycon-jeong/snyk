3f330fab06f9c6ea681552187d566cf1800f5f71
hoang <hoang@hyconsoft.com>
2021-10-29 10:19:12 +0900

    add main port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { useContainer } from 'class-validator';
 import { TrimStringsPipe } from './modules/common/transformer/trim-strings.pipe';
 import { AppModule } from './modules/main/app.module';
 import { setupSwagger } from './swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { APP_PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +16,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new TrimStringsPipe(), new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  await app.listen(3000);
+  await app.listen(APP_PORT || 3000);
 }
 bootstrap();
