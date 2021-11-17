09f900cd24f93b5fbbda21a5fea3a10ea9ce94bd
hoang-hyconsoft <82493575+hoang-hyconsoft@users.noreply.github.com>
2021-11-17 18:15:10 +0900

    change limit rate

--- src/main.ts
+++ src/main.ts
@@ -35,8 +35,8 @@ async function bootstrap() {
   // protect app from brute-force attacks
   app.use(
     rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 300, // limit each IP to 100 requests per windowMs
     }),
   );
   app.enableCors();
