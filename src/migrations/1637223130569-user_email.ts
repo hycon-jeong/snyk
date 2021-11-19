fe227183f9ebecf0a38232c2dd8ecafedb4fbbed
hoang <hoang@hyconsoft.com>
2021-11-19 08:01:34 +0000

    fix migration

--- src/migrations/1637223130569-user_email.ts
+++ src/migrations/1637223130569-user_email.ts
@@ -7,9 +7,9 @@ export class userEmail1637223130569 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE \`users\` ADD \`email\` varchar(255) NULL`,
     );
-    await queryRunner.query(
-      `ALTER TABLE \`users\` ADD UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`)`,
-    );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`users\` ADD UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`)`,
+    // );
     await queryRunner.query(
       `CREATE UNIQUE INDEX \`IDX_65cbf5fcb331619593ee334c7c\` ON \`users\` (\`email\`)`,
     );
@@ -19,9 +19,9 @@ export class userEmail1637223130569 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX \`IDX_65cbf5fcb331619593ee334c7c\` ON \`users\``,
     );
-    await queryRunner.query(
-      `ALTER TABLE \`users\` DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\``,
-    );
+    // await queryRunner.query(
+    //   `ALTER TABLE \`users\` DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\``,
+    // );
     await queryRunner.query(`ALTER TABLE \`users\` DROP COLUMN \`email\``);
   }
 }
