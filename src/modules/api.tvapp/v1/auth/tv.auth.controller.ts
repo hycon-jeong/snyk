1b20f98592e85eb28a2b2d32b2a9e0e828d6e3f5
jason <jaehyup.lee@hyconsoft.com>
2022-05-12 14:37:32 +0900

    Done:) 1. add log

--- src/modules/api.tvapp/v1/auth/tv.auth.controller.ts
+++ src/modules/api.tvapp/v1/auth/tv.auth.controller.ts
@@ -108,6 +108,7 @@ export class TvAuthController {
     const certCode = await this.getUniqueCertCode();
 
     const expireDt = moment().utc().add(5, 'minutes').toISOString();
+    console.log(expireDt);
 
     const res = await this.service.createTvCertCode({
       tvCertCode: certCode,
